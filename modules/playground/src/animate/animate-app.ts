d13f7e5421bad19c558ab3245b91aeb30b12a41c
Matias Niemelä <matias@yearofmoo.com>
2016-02-09 17:08:53 -0800

    chore(animate): remove all existing animation code

--- modules/playground/src/animate/animate-app.ts
+++ modules/playground/src/animate/animate-app.ts
@@ -3,9 +3,7 @@ import {Component} from 'angular2/core';
 @Component({
   selector: 'animate-app',
   template: `
-    <h1>The box is {{visible ? 'visible' : 'hidden'}}</h1>
-    <div class="ng-animate box" *ngIf="visible"></div>
-    <button (click)="visible = !visible">Animate</button>
+    ...
   `
 })
 export class AnimateApp {
